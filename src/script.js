16fa9c0b7af4fcc979952735869651ece9d3c44a
LionyxML <rahul.juliato@gmail.com>
2021-04-03 13:03:55 -0300

    Implemented multiple output options.

--- src/script.js
+++ src/script.js
@@ -3,11 +3,18 @@ var searchBox = document.getElementById('search-box');
 var consoleBox = document.getElementById('console');
 var apiSearch = 'https://swapi.dev/api/people/?search='
 
+var optRadio1 = document.getElementById('radio1');
+var optRadio2 = document.getElementById('radio2');
+var optRadio3 = document.getElementById('radio3');
 
-searchBox.addEventListener('input', function () {
-  //consoleBox.innerHTML = searchBox.value;
 
-  if (searchBox.value != '') {
+optRadio1.addEventListener('click', search);
+optRadio2.addEventListener('click', search);
+optRadio3.addEventListener('click', search);
+searchBox.addEventListener('input', search);
+
+function search() {
+  if ((searchBox.value != '') && (optRadio1.checked != true)){
     consoleBox.style.display = 'block';
     fetch(apiSearch + searchBox.value).then(
       response => response.json()).then(
@@ -16,16 +23,23 @@ searchBox.addEventListener('input', function () {
     } else {
       consoleBox.style.display = 'none';
     }
+}
 
-});
-
+var data = [];
 
 function getInfo(object) {
 
   cleanConsole();
+  data = [];
 
   object.forEach( function (element) {
-    printToConsole(element.name);
+      data.push(element);
+      if (optRadio2.checked == true) {
+          printToConsole(JSON.stringify(element.name));
+      }
+      if (optRadio3.checked == true) {
+        printToConsole(JSON.stringify(element));
+      }
   });
 
 }
